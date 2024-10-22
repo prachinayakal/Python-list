f1613fe2decc957cd90f82594a18607e6fbb96eb
Prachi Nayakal <121372605+prachinayakal@users.noreply.github.com>
2024-10-22 10:56:00 +0530

    Add files via upload

---
+++ list_basic.py
@@ -0,0 +1,11 @@
+# List 
+# Collection of a items
+list1 = [ "Rohan", "Physics", 21, 62.33]
+print(list1)
+
+# Accessing elements of a list
+list1 = [ "Rohan", "Physics", 21, 62.33]
+print(list1[0])
+
+list2 = [1,2,3,4,5,6,7]
+print(list2[1:5])
